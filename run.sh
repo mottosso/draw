b498fa27b20bc5f3f5e49327ca11fe10cf912169
Marcus Ottosson <konstruktion@gmail.com>
2017-11-07 09:41:30 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name draw -ti --rm -p 3002:9002 -v $(pwd):/workspace --entrypoint /workspace/bin/run.sh draw
+docker run --name draw -d --rm -p 3002:9002 -v $(pwd):/workspace --entrypoint /workspace/bin/run.sh draw
